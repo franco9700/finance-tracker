0f453cb8c50907f39cb2f5d997111d58eab9b698
franco9700 <francisco.iram97@gmail.com>
2020-04-03 18:07:43 -0700

    My portfolio view working and flash messages validation

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,6 +4,13 @@ class Stock < ApplicationRecord
 		client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_publishable_token],
 																  secret_token: Rails.application.credentials.iex_client[:sandbox_secret_token],
 																  endpoint: 'https://sandbox.iexapis.com/v1')
-		client.price(ticker_symbol)
+		begin
+			company = client.company(ticker_symbol)
+			price = client.price(ticker_symbol)
+			new(ticker: ticker_symbol, name: company.company_name, last_price: price)
+		rescue => exception
+			return nil
+		end
+		
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  get 'my-portfolio', to: 'users#my_portfolio'
+  get 'search-stock', to: 'stocks#search'
 end
