da99b2ef18c339124429af0109ca7d8b5eb973f7
franco9700 <francisco.iram97@gmail.com>
2020-04-06 17:49:48 -0700

    My friends view and navbar login-signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'my-portfolio', to: 'users#my_portfolio'
+  get 'my-friends', to: 'users#my_friends'
   get 'search-stock', to: 'stocks#search'
 end
